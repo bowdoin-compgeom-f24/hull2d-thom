b6a3f2572fec4849ed469656851ac3d59879df73
Tom Han <103209542+tom21100227@users.noreply.github.com>
2024-09-23 20:42:21 -0400

    rough idea of the HW

--- geom.cpp
+++ geom.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 #include <vector>
+#include <stack>
 
 using namespace std; 
 
@@ -12,7 +13,6 @@ using namespace std;
    is to the left of ab, and negative if c is to the right of ab
  */
 int signed_area2D(point2d a, point2d b, point2d c) {
-
   return 1; 
 }
 
@@ -21,8 +21,11 @@ int signed_area2D(point2d a, point2d b, point2d c) {
 /* **************************************** */
 /* return 1 if p,q,r collinear, and 0 otherwise */
 int collinear(point2d p, point2d q, point2d r) {
-  
-  return 1; 
+  double a_x = p.x - q.x;
+  double a_y = p.y - q.y;
+  double b_x = p.x - r.x;
+  double b_y = p.y - r.y;
+  return (a_x*b_y - a_y*b_x) == 0; 
 }
 
 
@@ -30,15 +33,21 @@ int collinear(point2d p, point2d q, point2d r) {
 /* **************************************** */
 /* return 1 if c is  strictly left of ab; 0 otherwise */
 int left_strictly(point2d a, point2d b, point2d c) {
-  
-  return 1; 
+  double a_x = a.x - b.x;
+  double a_y = a.y - b.y;
+  double b_x = a.x - c.x;
+  double b_y = a.y - c.y;
+  return (a_x*b_y - a_y*b_x) > 0; 
 }
 
 
 /* return 1 if c is left of ab or on ab; 0 otherwise */
 int left_on(point2d a, point2d b, point2d c) {
-
-  return 1; 
+  double a_x = a.x - b.x;
+  double a_y = a.y - b.y;
+  double b_x = a.x - c.x;
+  double b_y = a.y - c.y;
+  return (a_x*b_y - a_y*b_x) >= 0; 
 }
 
 
@@ -49,24 +58,39 @@ void graham_scan(vector<point2d>& pts, vector<point2d>& hull ) {
   printf("hull2d (graham scan): start\n"); 
   hull.clear(); //should be empty, but clear it to be safe
 
-  //just for fun: at the moment we set the hull as the bounding box of
-  //pts.  erase this and insert your code instead
-  int x1, x2, y1, y2;
-  if (pts.size() > 0) {
-    x1 = x2 = pts[0].x;
-    y1 = y2 = pts[0].y;
-    
-    for (int i=1; i< pts.size(); i++) {
-      if (pts[i].x < x1) x1 = pts[i].x;
-      if (pts[i].x > x2) x2 = pts[i].x;
-      if (pts[i].y < y1) y1 = pts[i].y;
-      if (pts[i].y > y2) y2 = pts[i].y;
+
+  // find p
+  point2d p = pts[0];
+  for (int i = 1; i < pts.size(); i++)
+  {
+    if (pts[i].y < p.y) p = pts[i];
+  }
+  printf("Lowest Point: (%d, %d)\n", p.x, p.y);
+
+  // Calculating angle while sorting, because I think the time it takes to 
+  // initialize a data structure that both a) points to my original point and 
+  // b) sortable probably would be more time consuming.
+  sort(pts.begin(), pts.end(),[p](const point2d& a_pt, const point2d& other_pt){
+    return atan2(a_pt.y - p.y, a_pt.x-p.x) < atan2(other_pt.y - p.y, other_pt.x-p.x);
+  });
+
+  // Super gross hack: in C++, atan2(0,0) returns 0, so my p would be at first. 
+
+  hull.push_back(p);
+  hull.push_back(pts[1]);
+  hull.push_back(pts[2]);
+
+  for (int i = 3; i < pts.size(); i++)
+  {
+    while (!left_strictly(hull[hull.size() - 2], hull.back(), pts[i]))
+    {
+      hull.pop_back();
     }
-    point2d p1 = {x1,y1}, p2 = {x2, y1}, p3 = {x2, y2}, p4 = {x1, y2}; 
-    hull.push_back(p1);
-    hull.push_back(p2);
-    hull.push_back(p3);
-    hull.push_back(p4);
+    hull.push_back(pts[i]);
+  }
+  for (size_t i = 0; i < hull.size(); i++)
+  {
+    printf("Point: (%d, %d)\n", hull[i].x, hull[i].y);
   }
   
   printf("hull2d (graham scan): end\n"); 
